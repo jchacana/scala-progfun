8024b219d6bfc45a41ca658fbec2e6cdb0bb7c0e
Javier Chacana <jchacana@exe.cl>
2016-12-28 21:41:35 -0800

    Sbt project for week 1

---
+++ progfun1-recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,56 @@
+package recfun
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+    def pascal(c: Int, r: Int): Int = {
+      if(c == 0 || (c == r)) 1
+      else pascal(c,r-1) + pascal(c-1,r-1)
+    }
+  
+  /**
+   * Exercise 2
+   */
+    def balance(chars: List[Char]): Boolean = {
+      
+      def loop(chars: List[Char], balanced: List[Char]): List[Char] = {
+        if(chars.isEmpty) balanced
+        else{
+        	if(chars.head == '(') {
+        	  loop(chars.tail, balanced.++(List('(')))
+        	}else if(chars.head == ')' && (!balanced.isEmpty && balanced.last == '(')) {
+        	  loop(chars.tail, balanced.take(balanced.length - 1))
+        	}else if(chars.head == ')') {
+        	  loop(chars.tail, balanced.++(List(')')))
+        	} else {
+        	  loop(chars.tail, balanced)
+        	}
+        }
+      }
+      if (loop(chars, List()).length == 0) true
+      else false
+    }
+  
+  /**
+   * Exercise 3
+   */
+    def countChange(money: Int, coins: List[Int]): Int = {
+      def intLoop(money: Int, coins: List[Int]): Int = {
+        if(money == 0) 1
+        else if(money < 0 ) 0
+        else if(coins.isEmpty) 0
+        else intLoop(money, coins.tail) + intLoop(money - coins.head, coins)
+      }
+      intLoop(money,coins)
+    }
+  }

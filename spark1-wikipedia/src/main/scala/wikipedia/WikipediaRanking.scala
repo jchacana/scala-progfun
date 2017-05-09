d1e98f885c600768d7b41d6806153ee87614909f
Javier Chacana <javierchacana@gmail.com>
2017-05-09 00:53:32 -0300

    First try 9/10, Second try 9/10, third try 10/10

--- spark1-wikipedia/src/main/scala/wikipedia/WikipediaRanking.scala
+++ spark1-wikipedia/src/main/scala/wikipedia/WikipediaRanking.scala
@@ -32,7 +32,8 @@ object WikipediaRanking {
    *  Hint2: consider using method `mentionsLanguage` on `WikipediaArticle`
    */
   def occurrencesOfLang(lang: String, rdd: RDD[WikipediaArticle]): Int = 
-    rdd.aggregate(0)((acc, w) => if(w.mentionsLanguage(lang)) 1 else 0 , _ + _)
+    rdd.filter(article => article.mentionsLanguage(lang)).count().toInt
+    //rdd.aggregate(0)((acc, w) => if(w.mentionsLanguage(lang)) 1 else 0 , _ + _)
 
   /* (1) Use `occurrencesOfLang` to compute the ranking of the languages
    *     (`val langs`) by determining the number of Wikipedia articles that
@@ -43,7 +44,7 @@ object WikipediaRanking {
    *   several seconds.
    */
   def rankLangs(langs: List[String], rdd: RDD[WikipediaArticle]): List[(String, Int)] = 
-    langs.map(lang => (lang, occurrencesOfLang(lang, rdd))).sortBy(e => e._2)
+    langs.map(lang => (lang, occurrencesOfLang(lang, rdd))).sortBy(e => -e._2)
 
   /* Compute an inverted index of the set of articles, mapping each language
    * to the Wikipedia pages in which it occurs.
